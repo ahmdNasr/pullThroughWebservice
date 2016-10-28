485639c0e5b0d231d3fca9d2174f4b7d9a4d49bc
Ahmed Nasr <nas15484@spengergasse.at>
2016-10-29 00:19:17 +0200

    removed typo

--- accesspatterns.js
+++ accesspatterns.js
@@ -6,7 +6,7 @@ module.exports = () => {
 			queries: ["SELECT user_id FROM users_login WHERE email = ? AND password = ?"],
 			description: "User login with basic authentication"
 		},
-		{ queries
+		{
 			name: "registration",
 			params: ["email", "username", "password"],
 			queries: ["FIND email, username, password;"]
