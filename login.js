acda7171187e95dbef5e1fb427af58e81f163270
Ahmed Nasr <nas15484@spengergasse.at>
2016-11-06 21:40:46 +0100

    added inital app declaration for login access pattern

---
+++ login.js
@@ -0,0 +1,21 @@
+
+/* ------------------- imports node modules -------------------*/
+const express    = require('express')
+const _          = require('underline')
+const bodyParser = require('body-parser')
+const cassandra  = require('cassandra-driver');
+const morgan     = require('morgan')
+
+
+/* ------------------- init variables -------------------*/
+const client = new cassandra.Client(config.db_connect)
+const app    = express()
+
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+
+
+
+app.listen(3000, () => console.log('server ready!'))
+
