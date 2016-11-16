e2170b3f92e997541b37e5ac83ab31489aa7df94
Ahmed Nasr <nas15484@spengergasse.at>
2016-11-16 10:26:26 +0100

    moved files to domain folder or domain/helpers

---
+++ domain/db.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var executeCQL = function(client, cql, params){
+
+	let deferedObject = Promise.defer()
+
+	client.execute(cql, params, (err, result) => {
+		err 
+		? deferedObject.reject(err)
+		: deferedObject.resolve(result.rowLength <= 1 ? result.rows[0] : result.rows)
+	})
+
+	return deferedObject.promise
+}
+
+
+
+exports.executeCQL = executeCQL
---
+++ domain/logic.js
@@ -0,0 +1,34 @@
+'use strict'
+const db = require('./db')
+
+
+// extracts the params from the request object in right order
+var extractParamsFromRequest = function(paramNames, req){
+	let paramsDefered = Promise.defer()
+	let params = []
+
+	paramNames.forEach( (paramName) => {
+		let paramValue = req.body[paramName] ? req.body[paramName] : paramsDefered.reject(new Error(`Parameter ${paramName} not found in request object.`))
+		params.push(paramValue)
+	})
+
+	paramsDefered.resolve(params)
+	return paramsDefered.promise
+}
+
+var stdSelect = function(request, accesspattern, dbclient){
+	let selectDefered = Promise.defer()
+	
+	let cql = accesspattern.query || accesspattern.queries[0]
+	
+	extractParamsFromRequest(accesspattern.params, request).then( (params) => {
+		db.executeCQL(dbclient, cql, params)
+		  .then(selectDefered.resolve)
+		  .catch(selectDefered.reject)
+	}).catch(selectDefered.reject)
+	
+	return selectDefered.promise
+}
+
+
+exports.stdSelect = stdSelect
