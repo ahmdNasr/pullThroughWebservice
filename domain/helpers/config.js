728fdbf1606a5598c036d8d034be6014bba1349c
Ahmed Nasr <nas15484@spengergasse.at>
2016-11-17 00:38:53 +0100

    exporting config the right way

--- domain/helpers/config.js
+++ domain/helpers/config.js
@@ -2,13 +2,10 @@ const cassandra = require ('cassandra-driver')
 const PlainTextAuthProvider = cassandra.auth.PlainTextAuthProvider 
 
 
-module.exports = (() => {
-	return {
-		db_connect: { 
-			contactPoints: ['195.88.35.57','195.88.35.61','195.88.35.64'], 
-			keyspace: 'comundo',
-			authProvider: new PlainTextAuthProvider('std_harl', 'NFJgPa2-fHel15b_K4v7jY3mpGASTQM0' )
-		},
-		other: { some: "config"}
-	}
+const db_connect = { 
+	contactPoints: ['195.88.35.57','195.88.35.61','195.88.35.64'], 
+	keyspace: 'comundo',
+	authProvider: new PlainTextAuthProvider('std_harl', 'NFJgPa2-fHel15b_K4v7jY3mpGASTQM0' )
 }
+
+exports.db_connect = db_connect
