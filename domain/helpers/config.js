387e49a4aead266823db36aaa00d3c2018fa601e
Ahmed Nasr <nas15484@spengergasse.at>
2016-11-22 19:50:02 +0100

    moved logger.js config to config.js

--- domain/helpers/config.js
+++ domain/helpers/config.js
@@ -8,4 +8,19 @@ const db_connect = {
 	authProvider: new PlainTextAuthProvider('std_harl', 'NFJgPa2-fHel15b_K4v7jY3mpGASTQM0' )
 }
 
+const log4js = {
+	appenders: [
+		{
+			type: 'log4js-ain2',
+			tag: 'webservice',
+			facility: 'local4',
+			hostname: '195.88.35.64',
+			port: 12203,
+			transport: 'UDP',
+			password:"NFJgPa2-fHel15b_K4v7jY3mpGASTQM0"
+		}
+	]
+}
+
 exports.db_connect = db_connect
+exports.log4js = log4js
--- domain/helpers/logger.js
+++ domain/helpers/logger.js
@@ -1,18 +1,7 @@
 const log4js = require('log4js')
 const config = require('./config.js')
-log4js.configure({
-	appenders: [
-		{
-			type: 'log4js-ain2',
-			tag: 'webservice',
-			facility: 'local4',
-			hostname: '195.88.35.64',
-			port: 12203,
-			transport: 'UDP',
-			password:"NFJgPa2-fHel15b_K4v7jY3mpGASTQM0"
-		}
-	]
-})
+
+log4js.configure(config.log4js)
 
 var logger = log4js.getLogger('nodejs-v6.9.1')
 
