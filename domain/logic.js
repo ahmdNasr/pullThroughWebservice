bac0cab1b2e5c61c1366d649f5705a70f7054b77
Ahmed Nasr <nas15484@spengergasse.at>
2017-01-20 08:15:19 +0100

    added JWT code (and basic authentication - again)

--- domain/logic.js
+++ domain/logic.js
@@ -1,5 +1,6 @@
 'use strict'
 const db = require('./db')
+const jwt = require('jsonwebtoken')
 
 
 // extracts the params from the request object in right order
@@ -16,12 +17,17 @@ var extractParamsFromRequest = function(paramNames, req){
 	return paramsDefered.promise
 }
 
+var basicAuthenticate = function(inputEmail, inputPasswort, db){
+	
+}
+
 var stdSelect = function(request, accesspattern, dbclient){
 	let selectDefered = Promise.defer()
-	
+
 	let cql = accesspattern.query || accesspattern.queries[0]
 	
 	extractParamsFromRequest(accesspattern.params, request).then( (params) => {
+	
 		db.executeCQL(dbclient, cql, params)
 		  .then(selectDefered.resolve)
 		  .catch(selectDefered.reject)
@@ -30,5 +36,44 @@ var stdSelect = function(request, accesspattern, dbclient){
 	return selectDefered.promise
 }
 
+var loginJWT =  function(req, accesspattern, dbclient){
+	let loginDefered = Promise.defer()
+
+	let slectCql = accesspattern.queries[0] // Select user_id....
+	
+	const authenticate = (dbResult) => {
+		let authDefered = Promise.defer()
+		
+		req.body.email, req.body[password], dbResult.email, dbResult.password
+
+			if(req.body.email == req.body[password] && dbResult.email == dbResult.password){
+
+			let token = jwt.sign(user, config.jwtSecret, {
+				expiresInMinutes: 1440 // expires in 24 hours
+			})
+
+			authDefered.resolve({
+				success: true,
+				message: 'Enjoy your token!',
+				token: token
+			})
+
+		}
+		return authDefered.promise
+	}
+	
+	stdSelect(req, accesspattern, dbclient)
+			.then(authenticate) // authenticate resolves token token with user data
+			.then(selectDefered.resolve)
+			.catch(selectDefered.reject)
+
+
+	return loginDefered.promise
+}
+
+
+/*var login = function(req, accesspattern, dbclient){
+	le
+}*/
 
 exports.stdSelect = stdSelect
