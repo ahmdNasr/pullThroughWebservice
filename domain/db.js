cca634b7de71f4b6276b518b0d4406c9d9f8cc42
Ahmed Nasr <nas15484@spengergasse.at>
2017-02-01 16:53:49 +0100

    set cql statements prepare to true

--- domain/db.js
+++ domain/db.js
@@ -1,10 +1,10 @@
 'use strict'
 
 var executeCQL = function(client, cql, params){
-
+console.log(params)
 	let deferedObject = Promise.defer()
 
-	client.execute(cql, params, (err, result) => {
+	client.execute(cql, params,  { prepare: true }, (err, result) => {
 		err 
 		? deferedObject.reject(err)
 		: deferedObject.resolve(result.rowLength <= 1 ? result.rows[0] : result.rows)
