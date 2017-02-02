a148e09f2fe923e9ed0595b8dd8ce54016c8dc05
Ahmed Nasr <nas15484@spengergasse.at>
2017-02-02 15:42:19 +0100

    cleaned up api.js

--- domain/api.js
+++ domain/api.js
@@ -1,40 +1,18 @@
 'use strict'
 const express = require('express')
 const logic = require('./logic')
+const middleware = require('./middleware')
 
 const loggerFactory = require('./helpers/logger.js')
 const apiErrorLogger = loggerFactory("ERROR")
-
-const loginAccessPattern = {
-	params: ["email", "password"],
-	queries: ["SELECT user_id, firstname, lastname, username FROM users_by_email WHERE email = ? AND password = ?;"]
-}
-
-var doBasicAuth = function(data){
-	let authDefered = Promise.defer()
-	console.log("doBasicAuth"+ data)
-	data 
-	? authDefered.resolve() 
-	: authDefered.reject({ status: 500, errorMessage: "This query needs authentication!" })
-
-	return authDefered.promise
-}
 
 var generateRouter = function(accesspattern, dbclient){
 	let genRouterDefered = Promise.defer()
 	let router = express.Router()
 
-	if(accesspattern.authRequired){
-		router.use( (req, res, next) => {
-			logic.stdSelect(req, loginAccessPattern, dbclient)
-			.then(doBasicAuth)
-			.then(next)
-			.catch((error) => {
-				res.status(401).json(error).end() // 401 -> Unauthorized
-				//apiErrorLogger(`Error on Request: ${error}`)
-			})
-		})
-	}
+	if(accesspattern.authRequired)
+		router.use(middleware.authentication)
+	
 	
 	router[accesspattern.method.toLowerCase()]( `/${accesspattern.name}`, (req, res) => {
 		let logichandler = logic[accesspattern.name] || logic.stdSelect
